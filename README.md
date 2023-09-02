6aa293f1cd7bfc220bb18540269e33c1df9b9cf1
Marcos Joel Soriano Guerrero <89612207+MARCOSJOEL25@users.noreply.github.com>
2023-09-02 08:38:00 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# learningpath
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/learningpath/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.learningpath
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
