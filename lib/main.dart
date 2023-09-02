e59a2ef8dbc85141a08015dd37f174aba2b3a589
Marcos Joel Soriano Guerrero <89612207+MARCOSJOEL25@users.noreply.github.com>
2023-09-02 09:30:09 -0400

    navigator

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,38 @@
 import 'package:flutter/material.dart';
 
-class myAppBar extends StatelessWidget {
-  final Widget title;
-
-  const myAppBar({super.key, required this.title});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 56.0,
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: BoxDecoration(color: Colors.blue[500]),
-      child: Row(children: <Widget>[
-        IconButton(
-          onPressed: null,
-          icon: Icon(Icons.menu),
-        ),
-        Expanded(child: title),
-        IconButton(
-          onPressed: null,
-          icon: Icon(Icons.search),
-        )
-      ]),
+void main() => runApp(
+      const MaterialApp(
+        home: MyNavigator(),
+        title: 'Flutter tutorial',
+      ),
     );
-  }
-}
 
-class myScaffold extends StatelessWidget {
+class MyNavigator extends StatelessWidget {
+  const MyNavigator({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Column(
-        children: [
-          SafeArea(
-            child: myAppBar(
-              title: Text(
-                'Example title',
-                style: Theme.of(context).primaryTextTheme.titleMedium,
-              ),
-            ),
-          ),
-          const Expanded(
-            child: Center(
-              child: Text("Hello World!"),
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        leading: const IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: null,
+        ),
+        title: const Text("Titulo"),
+        actions: const [
+          IconButton(
+            onPressed: null,
+            icon: Icon(Icons.search),
           )
         ],
       ),
+      body: const Center(
+        child: Text("Hello, World!"),
+      ),
+      floatingActionButton: const FloatingActionButton(
+        onPressed: null,
+        child: Icon(Icons.add),
+      ),
     );
   }
-}
-
-void main() {
-  runApp(MaterialApp(
-    title: 'my App',
-    home: myScaffold(),
-  ));
 }
